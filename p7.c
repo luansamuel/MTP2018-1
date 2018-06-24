18c53cbd9686973c6f7fbb7bc9027b7c690ac119
Luan Mendes <37585695+luansamuel@users.noreply.github.com>
2018-06-24 20:24:23 -0300

    Update and rename p7.txt to p7.c

---
+++ p7.c
@@ -0,0 +1,27 @@
+    /* Luan Samuel Lopes Mendes 
+11621ETE016
+
+
+*/
+#include <stdio.h>
+
+int a (int m, int n)
+{
+	if(m==0)
+		return n+1;
+	else if(m>0 && n==0)
+		return a(m-1, 1);
+	else if(m>0 && n>0)
+		return a(m-1, a(m, n-1));
+}
+
+int main ()
+{
+	int m, n;
+	printf("Digite m: ");
+	scanf("%d", &m);
+	printf("\nDigite n: ");
+	scanf("%d", &n);
+	printf("%d", a(m, n));
+	return 0;
+}
