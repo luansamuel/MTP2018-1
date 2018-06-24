a7010b4abab097665ac450c555093e9cde6ae005
Luan Mendes <37585695+luansamuel@users.noreply.github.com>
2018-06-24 20:16:05 -0300

    Update p1.c

--- p1.c
+++ p1.c
@@ -5,29 +5,39 @@
 */
 
 
-#include <stdio.h>
 
-int main() {
-    int estado=0,i;
-    char bits[256];
+#include  < stdio.h >
 
-    scanf("%s",&bits);
-    while(bits[i] != '\0'){
-        if(estado == 0 && bits[i] == '0'){
-        estado=0;
-        i++;
-    } else{
-            estado=1;
-    }}
-    printf("estado= %d \n",estado);
-    printf("Sequencia original = %s ",bits);
-    if(estado==0){
-            printf("e multiplo de 3");
-             }
-    else{
-         printf("nao e");
-         }
-
-    return 0;
-
+int  main () {
+    int estado = 0 , i, a = 0 ;
+    bits de char [ 256 ], ibits [ 256 ];
+    printf ( " Por favor, digite uma sequencia de 1's e 0's: " );
+    fflush (stdin);
+    scanf ( " % s " , ( char *) e bits);
+    para (i = 0 ; bits [i]! = ' \ 0 ' ; i ++) {
+        a + = 1 ;
     }
+    para (i = a- 1 ; i> = 0 ; i -) {
+        ibits [a- 1 -i] = bits [i];
+    }
+    ibits [a] = ' \ 0 ' ;
+    i = 0 ;
+    while (ibits [i]! = ' \ 0 ' ) {
+        if (estado == 0 ) {
+            if (ibits [i] == ' 1 ' )
+                estado + = 1 ;
+        } else  if (estado == 1 ) {
+            if (ibits [i] == ' 0 ' ) {
+                estado + = 1 ;
+            } mais {
+                estado- = 1 ;
+            }
+        } mais {
+            if (ibits [i] == ' 0 ' )
+                estado- = 1 ;
+        }
+        i + = 1 ;
+    }
+    printf ( " Estado: % d \ n Uma sequencia % s  % s eh multipla de 3 " , estado, bits, (estado! = 0 ? " nao " : " " ));
+    return  0 ;
+}
