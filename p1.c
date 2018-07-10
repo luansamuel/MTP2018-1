0bcc193f05733169791241457fd392f0b24416f4
Luan Mendes <37585695+luansamuel@users.noreply.github.com>
2018-07-10 18:46:26 -0300

    Update p1.c

--- p1.c
+++ p1.c
@@ -3,41 +3,47 @@
 
 
 */
-
-
-
-#include  <stdio.h>
-
-int  main () {
-    int estado = 0 , i, a = 0 ;
-    bits de char [ 256 ], ibits [ 256 ];
-    printf ( " Por favor, digite uma sequencia de 1's e 0's: " );
-    fflush (stdin);
-    scanf ( " % s " , ( char *) e bits);
-    para (i = 0 ; bits [i]! = ' \ 0 ' ; i ++) {
-        a + = 1 ;
-    }
-    para (i = a- 1 ; i> = 0 ; i -) {
-        ibits [a- 1 -i] = bits [i];
-    }
-    ibits [a] = ' \ 0 ' ;
-    i = 0 ;
-    while (ibits [i]! = ' \ 0 ' ) {
-        if (estado == 0 ) {
-            if (ibits [i] == ' 1 ' )
-                estado + = 1 ;
-        } else  if (estado == 1 ) {
-            if (ibits [i] == ' 0 ' ) {
-                estado + = 1 ;
-            } mais {
-                estado- = 1 ;
-            }
-        } mais {
-            if (ibits [i] == ' 0 ' )
-                estado- = 1 ;
-        }
-        i + = 1 ;
-    }
-    printf ( " Estado: % d \ n Uma sequencia % s  % s eh multipla de 3 " , estado, bits, (estado! = 0 ? " nao " : " " ));
-    return  0 ;
+#include <stdio.h>
+int main()
+{
+	int est=0, i=0, j=0;
+	char bits[256];
+	while (j>=0)
+	{
+		j=0;
+		printf("Insira um numero valido para que seja determinado se este e multiplo 3: ");
+	    scanf("%s", bits);
+	    for (i=0; bits[i]!='\0'; i++)
+	    {
+	    	if (bits[i]!='0' && bits[i]!='1')
+	    	j++;
+		}
+		j--;
+	}
+	for (i=0; bits[i]!='\0'; i++)
+	{
+		if (bits[i]=='0')
+		{
+			if (est==0)
+				est=0;
+			else if (est==2)
+			    est=1;
+			else
+			    est=2;
+		}
+		else if (bits[i]=='1')
+		{
+			if (est==2)
+			    est=2;
+			else if (est==0)
+			    est=1;
+			else
+			    est=0;
+		}
+	}
+	if (est==0)
+	    printf("\n%s e um multiplo de 3", bits);
+	else
+	    printf("\n%s nao um multiplo de 3", bits);
+	return 0;
 }
